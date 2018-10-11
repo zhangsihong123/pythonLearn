f147b9d3beb8044ff2346e20b415c55befb243ff
zhangsihong123 <azsh19941014>
2018-10-11 18:06:16 +0800

    python 学习

--- python_function.py
+++ python_function.py
@@ -146,3 +146,95 @@ def product(x, *y):
 #         print('测试失败!')
 #     except TypeError:
 #         print('测试成功!')
+
+#递归函数
+def fact(num,product):
+	if num == 1:
+		return product
+	return fact(num-1,num*product)
+#print(fact(5,1))
+
+#######################################高级特性############################################
+#切片学习
+#一下代码可以实现获取表单中某一段数据，但是这样还是太麻烦
+L = ['Michael','Sarah','Tracy','Bob','Jack']
+r = []
+n = 4
+for i in range(n):
+	r.append(L[i])
+print('r:',r)
+#python中提供了切片Slice操作符,同时也支持倒切片,这里有点屌 竟然可以越界
+L[0:3]
+L[1:4]
+L[2:5]
+L[3:9]
+L[-5:-1]
+#所有元素中每隔100个取一个
+L[::100]
+#前100个元素中每隔20个取一个
+L[:100:20]
+#甚至什么都不写可以原样复制一个list出来
+L[:]
+#以此类推tuple也同样可以进行上述操作 只不过tuple是不变的集合
+T=('I','love','python')
+T[0:2]
+T[1:3]
+T[::2]
+T[:3:2]
+#同样字符串也可以使用切片
+S = 'hangzhouxihu'
+S[0:len(S)]
+S[:3]#取前面三个字符
+S[::2]#所有字符中每隔2个取一个
+S[:10:2]#前面10个字符中每隔2个取一个
+
+####################################高级特性###############################
+#迭代
+D = {'name':'zsh','age':12,'gender':'man'}
+#迭代key
+for key in D.keys():
+	print(key,":",D.get(key))
+#迭代value
+for vlaue in D.vlaues():
+	print(vlaue)
+#同时迭代key和value, 这个真是牛批
+for key,vlaue in D.items():
+	print(key,":",value)
+
+#类似字符串也可以迭代遍历
+S = 'I go home bilibilibili'
+for s in S:
+	print('char:',s)
+#是否是可迭代对象通过collections模块的Iterable类型判断
+#示例
+from collections import Iterable
+isinstance('可迭代对象(判断对象)',Iterable)
+
+#同时迭代list 下标和值
+M = [(1,2),(2,4),(3,5)]
+for i,v in enumerate(L):
+	print(i,v)
+#同时引用两个变量
+for x,y in M:
+	print(x,y)
+
+#练习
+#请使用迭代查找一个list中最小和最大值，并返回一个tuple：
+#这个函数还没用
+def findMaxAndMin(L):
+	if L!=[]:
+		min=L[0]
+		max=L[0]
+		for n in L:
+			if n<min:
+				min = n
+			if n>max:
+				max = n
+		return (min,max)
+	else:
+		return (None,None)
+
+#list生成式
+[x*x for x in range(1,100)]
+[x*x for x in range(1,100) if x%2=0]
+[m + n for m in 'ABC' for n in 'XYZ']
